dacd6869edcbb861457ed74ace4e6404e35dbf67
Ivan Zavorykin <zavorykin.dev@yandex.ru>
2024-06-18 23:09:16 +0300

    sprint 8: переоформил на единый тест-файл для простоты проведения тестов

--- test/FileBackedTaskManagerTest.java
+++ test/FileBackedTaskManagerTest.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.*;
 import task.managers.file_manager.FileBackedTaskManager;
 
 import java.nio.file.Path;
-import java.util.List;
 
 public class FileBackedTaskManagerTest extends TaskManagersTest<FileBackedTaskManager> {
 
@@ -27,13 +26,11 @@ public class FileBackedTaskManagerTest extends TaskManagersTest<FileBackedTaskMa
     @Test
     @DisplayName("Проверка загрузки из файла с историей")
     public void loadFromFileWithHistoryTest() {
-        Path fileWithHistoryPath = Path.of("test/FileBackedTest_2.csv");
-        FileBackedTaskManager testManager = FileBackedTaskManager.load(fileWithHistoryPath.toFile());
+        FileBackedTaskManager testManager = FileBackedTaskManager.load(path.toFile());
 
-        Assertions.assertEquals(3,testManager.getHistory().size());
-        Assertions.assertEquals(List.of(testManager.getTask(1),
-                        testManager.getEpic(3),
-                        testManager.getSubtask(5)),
-                testManager.getHistory());
+        Assertions.assertEquals(1,testManager.getHistory().size());
+        Assertions.assertEquals(2,testManager.getAllTasks().size());
+        Assertions.assertEquals(1,testManager.getAllEpicTasks().size());
+        Assertions.assertEquals(2,testManager.getAllSubtasks().size());
     }
 }
